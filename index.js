f00feb8055dedf8fc4b4cc9a3a1b4002028e1da2
Tuğcan Özaktaş <tugcanozaktas@hotmail.com>
2023-03-02 18:15:47 +0200

    Added the pop-up and clear properties o the form.

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 window.onload = function(){
-    alert("Hello WOrld!");
-
+    submitForm();
+    clearForm();
 }
 
+
+function submitForm() {
+    var alerter = function(e){
+        e.preventDefault();
+        
+        var alerterText = document.getElementById("name-input").value + " " + document.getElementById("email-input").value + " " + document.getElementById("message").value;
+
+        alert(alerterText);
+    }
+
+
+    var submitButton = document.querySelector("#submit-button");
+
+    submitButton.addEventListener("click", alerter, false);
+}
+
+function clearForm(){
+    var clearFunc = function(e){
+        e.preventDefault();
+
+        var nameInput = document.getElementById("name-input");
+
+        var emailInput = document.getElementById("email-input");
+
+        var messageInput = document.getElementById("message");
+        
+        nameInput.value = "";
+        emailInput.value = "";
+        messageInput.value = "";
+    }
+    
+    
+    
+    var clearButton = document.querySelector("#clear-button");
+    
+    clearButton.addEventListener("click", clearFunc, false);
+}
