06555540e10243c12ef49ed6febdc5e9d9806002
Tuğcan Özaktaş <tugcanozaktas@hotmail.com>
2023-03-02 17:28:14 +0200

    same things

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-var submitButton = document.getElementById("submit-button");
-submitButton.addEventListener("click", alertFunc, false)
+window.onload = function(){
+    alert("Hello WOrld!");
 
-var alertFunc = function(){
-    var fullName = document.getElementById("name-input").value;
-    var email = document.getElementById("email-input").value;
-    var message = document.getElementById("message").value;
-
-    var alert = fullName + email + message ;
-
-    alert(alert);
 }
 
